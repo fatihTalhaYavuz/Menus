057ef7727a15b7bf55ac95800161dcabde928f3e
FATİH TALHA YAVUZASLANOĞLU <95251900+fatihTalhaYavuz@users.noreply.github.com>
2023-01-23 01:39:57 +0300

    data changed

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -12,31 +12,31 @@ class Products with ChangeNotifier {
       id: '31',
       title: 'yellow Shirt',
       description: 'A blue shirt - it is perfectly red!',
-      price: 29.99,
+      price: 29.9990,
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'g2',
       title: 'Trouserss',
-      description: 'A perfect to pair of trousers.',
-      price: 43.99,
+      description: 'A perfect kklşjşjş to pair of trousers.',
+      price: 43.98909,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'g3',
       title: 'Yellow Scarff',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 76.99,
+      description: 'Warm and cozlklky - exactly what you need for the winter.',
+      price: 76.9909,
       imageUrl:
           'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'g4',
       title: 'A Pann',
-      description: 'Prepare any meal you wantt.',
-      price: 76.99,
+      description: 'Preparemlkm any meal you wantt.',
+      price: 76.909,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
