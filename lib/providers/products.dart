07b46a813dfc83eef4d4335a85bb6e25bb6c85df
FATİH TALHA YAVUZASLANOĞLU <95251900+fatihTalhaYavuz@users.noreply.github.com>
2023-03-30 01:33:46 +0300

    data changed

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -18,27 +18,27 @@ class Products with ChangeNotifier {
     ),
     Product(
       id: 'g3',
-      title: 'Trouuuuuuuuuuseeersssssdsd',
+      title: 'Trouuueeersssssdsd',
       description: 'A perfect kklşjşjş to pair of trousers.',
       price: 43.98909,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'g4',
-      title: 'Yellow Scarrrrfffffff',
+      title: 'Yellow Scarrrfffff',
       description: 'Warm and cozlklky - exactly what you need for the winter.',
       price: 76.9909,
       imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+      'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'g4',
       title: 'A Pannnnnn',
-      description: 'Prepaaaaareeeeeemlllkkm any meal you wwwwantt.',
+      description: 'Prepaareeeeeemlllkkm any meal you wwwwantt.',
       price: 76.909,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
 
